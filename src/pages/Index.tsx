4fcc105db1c14e57b3a0634420418be76aa2898f
rajpootabubakar43215-afk <rajpootabubakar43215@gmail.com>
2026-04-22 11:55:22 +0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,8 +8,7 @@ import { LiveStatusBar } from "@/components/LiveStatusBar";
 import { AdminLeaderboard } from "@/components/AdminLeaderboard";
 import { ReasonsAnalytics } from "@/components/ReasonsAnalytics";
 import { PlayerLookup } from "@/components/PlayerLookup";
-import { ServerStatusWidget } from "@/components/ServerStatusWidget";
-import { DiscordWidget } from "@/components/DiscordWidget";
+
 import { cn } from "@/lib/utils";
 
 const Index = () => {
